8623e611991ecc0828f8aa0c2566179f5a7f73c0
dkckacka1 <125544460+dkckacka1@users.noreply.github.com>
2023-02-18 18:44:42 +0900

    Kimhawon
    Facade

---
+++ R&D Project/Assets/Pattern/Facade/Facade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Pattern.Facade
+{
+    public class Facade : MonoBehaviour
+    {
+        public void TurnOnAction()
+        {
+            print("수많은 작업 그리고 TurnOn");
+        }
+
+        public void TurnOffAction()
+        {
+            print("수많은 작업 그리고 TurnOff");
+        }
+
+    }
+}
---
+++ R&D Project/Assets/Pattern/Facade/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Pattern.Facade
+{
+    public class Main : MonoBehaviour
+    {
+        public Facade facade;
+
+        private void Awake()
+        {
+            facade = GetComponent<Facade>();
+        }
+
+        public void TurnOn()
+        {
+            print($"메인에서는 함수하나만 호출하면 된다.");
+            facade.TurnOnAction();
+        }
+
+        public void TurnOff()
+        {
+            print($"메인에서는 함수하나만 호출하면 된다.");
+            facade.TurnOffAction();
+        }
+    }
+}
