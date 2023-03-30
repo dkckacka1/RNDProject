953240548bedb91ccbe603969f4a06bf36d0c647
dkckacka1 <125544460+dkckacka1@users.noreply.github.com>
2023-03-30 13:54:24 +0900

    Kimhawon
    Homework1

---
+++ Homework/UnityPractice_1/Assets/02. Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int gold;
+    public int Combo;
+    public int heart = 5;
+
+    public Text GoldText;
+    public Text ComboText;
+
+    public RawImage[] heartImages;
+
+
+    #region ButtonPlugin
+
+
+
+    #endregion
+}
---
+++ Homework/UnityPractice_1/Assets/02. Scripts/RythumButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RythumButton : MonoBehaviour
+{
+    public GameObject rythumNodePrefab;
+    public GameObject node;
+    public Transform nodeSpawnPosition;
+    public float nodeSpeed = 1f;
+    public Text comboText;
+    public Vector3 magnitude;
+    private void Start()
+    {
+        node = Instantiate(rythumNodePrefab, nodeSpawnPosition.position, Quaternion.identity, this.gameObject.transform);
+        magnitude = (this.transform.position - nodeSpawnPosition.position).normalized;
+    }
+
+    public void Update()
+    {
+        if (node != null)
+        {
+            node.transform.position += magnitude * Time.deltaTime * nodeSpeed;
+            if (node.transform.position.y < -80)
+            {
+                Destroy(node.gameObject);
+            }
+        }
+    }
+
+    public void ButtonDown()
+    {
+        if (node != null)
+        {
+
+            
+        }
+    }
+}
