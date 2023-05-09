3b56d9aa173f8575c0163df64ecc4aec4bf544be
dkckacka1 <125544460+dkckacka1@users.noreply.github.com>
2023-05-09 13:50:08 +0900

    Kimhawon
    RND Inventory

---
+++ R&D Project/Assets/Unity/Inventory/Default/Script/UI/MovableHaderUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Inventory.Default
+{
+    public class MovableHaderUI : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+}
