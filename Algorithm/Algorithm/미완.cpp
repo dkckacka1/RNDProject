346f24bb49d621266160208a56c97f3a9f8daa34
dkckacka1 <125544460+dkckacka1@users.noreply.github.com>
2023-08-20 21:12:25 +0900

    Kimhawon
    개인 R&D

---
+++ Algorithm/Algorithm/미완.cpp
@@ -0,0 +1,48 @@
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//vector<vector<int>> solution(int n)
+//{
+//    vector<vector<int>> answer;
+//
+//    for (int i = 0; i < n - 1; i++)
+//    {
+//        answer.push_back({ 1,2 });
+//    }
+//    answer.push_back({ 1,3 });
+//    for (int i = 0; i < n - 1; i++)
+//    {
+//        answer.push_back({ 2,3 });
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    auto answer = solution(1);
+//
+//    for (auto v1 : answer)
+//    {
+//        for (auto i : v1)
+//        {
+//            cout << i << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    return 0;
+//}
+//
+///*
+//
+//1,2
+//1,2
+//1,3
+//2,3
+//2,3
+//
+//*/
